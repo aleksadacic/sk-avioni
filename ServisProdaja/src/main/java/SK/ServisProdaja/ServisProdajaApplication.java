e4b6107dbd51f94222ce770c1744afe823356fe5
aleksadacic <aleksa.dacic99@gmail.com>
2020-12-20 00:45:12 +0100

    init

---
+++ ServisProdaja/src/main/java/SK/ServisProdaja/ServisProdajaApplication.java
@@ -0,0 +1,13 @@
+package SK.ServisProdaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisProdajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisProdajaApplication.class, args);
+	}
+
+}
---
+++ ServisProdaja/src/test/java/SK/ServisProdaja/ServisProdajaApplicationTests.java
@@ -0,0 +1,13 @@
+package SK.ServisProdaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisProdajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
